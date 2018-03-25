2368431962aa6d6fbf6c25a33e55e7dfe9cff6db
Mr.Huang <18757163093@163.com>
2018-03-25 13:04:41 +0800

    Create L1-005. 考试座位号.cpp

---
+++ GPLT Practice/L1-005. 考试座位号.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct stu {
+string s;
+int one;
+int two;
+};
+int main()
+{
+    int n;
+    cin>>n;
+    stu *a=new stu[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i].s;
+        cin>>a[i].one;
+        cin>>a[i].two;
+    }
+    int m;
+    cin>>m;
+    int temp;
+    for(int i=0;i<m;i++)
+    {
+        cin>>temp;
+        for(int j=0;j<n;j++)
+        {
+            if(a[j].one==temp)
+            {
+                cout<<a[j].s<<" ";
+                cout<<a[j].two<<endl;
+              //  break;
+            }
+        }
+    }
+    return 0;
+}

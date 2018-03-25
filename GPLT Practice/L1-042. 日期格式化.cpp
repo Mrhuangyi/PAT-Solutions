612dd314c84e07f817e341250b163658d06f6720
Mr.Huang <18757163093@163.com>
2018-03-25 13:23:48 +0800

    Create L1-042. 日期格式化.cpp

---
+++ GPLT Practice/L1-042. 日期格式化.cpp
@@ -0,0 +1,10 @@
+#include<cstdio>
+#include<iostream>
+using namespace std;
+int main()
+{
+    char s[10];
+    scanf("%s",s);
+    printf("%c%c%c%c%c%c%c%c%c%c",s[6],s[7],s[8],s[9],s[5],s[0],s[1],s[2],s[3],s[4]);
+    return 0;
+}

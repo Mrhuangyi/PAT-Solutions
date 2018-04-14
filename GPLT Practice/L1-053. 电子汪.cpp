93e631efdb7d9cd2a582691fa633ba330a854527
Mr.Huang <18757163093@163.com>
2018-04-14 10:53:42 +0800

    Create L1-053. 电子汪.cpp

---
+++ GPLT Practice/L1-053. 电子汪.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main(){
+    int a,b;
+    string s="Wang!";
+    scanf("%d%d",&a,&b);
+    int c=a+b;
+    while(c--){
+        cout<<s;
+    }
+    return 0;
+}

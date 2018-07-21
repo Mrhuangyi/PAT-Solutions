0f0ba73385af45e573e4c7ef4ebcb3b0efc1464f
Mr.Huang <18757163093@163.com>
2018-07-21 13:55:48 +0800

    Create 1027 Colors in Mars (20)（20 分）.cpp

---
+++ Advanced Level- Practice/1027 Colors in Mars (20)（20 分）.cpp
@@ -0,0 +1,16 @@
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+using namespace std;
+char radix[13] ={
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C'
+};
+int main() {
+    int r, g, b;
+    scanf("%d%d%d", &r, &g, &b);
+    printf("#");
+    printf("%c%c", radix[r / 13], radix[r % 13]);
+    printf("%c%c", radix[g / 13], radix[g % 13]);
+    printf("%c%c", radix[b / 13], radix[b % 13]);
+    return 0;
+}

864eb27a0f53aad5a11c6ca4956c7d4e59951ad2
Mr.Huang <18757163093@163.com>
2018-07-21 13:05:49 +0800

    Create 1031 Hello World for U (20)（20 分）.cpp

---
+++ Advanced Level- Practice/1031 Hello World for U (20)（20 分）.cpp
@@ -0,0 +1,32 @@
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+using namespace std;
+int main() {
+    char str[100], ans[40][40];
+    scanf("%s", str);
+    int len = strlen(str);
+    int n1 = (len + 2) / 3, n3 = n1, n2 = len + 2 - n1 - n3;
+    for(int i = 1; i <= n1; i++) {
+        for(int j = 1; j <= n2; j++) {
+            ans[i][j] = ' ';
+        }
+    }
+    int pos = 0;
+    for(int i = 1; i <= n1; i++) {
+        ans[i][1] = str[pos++];
+    }
+    for(int j = 2; j <= n2; j++) {
+        ans[n1][j] = str[pos++];
+    }
+    for(int i = n3 - 1; i >= 1; i--) {
+        ans[i][n2] = str[pos++];
+    }
+    for(int i = 1; i <= n1; i++) {
+        for(int j = 1; j <= n2; j++) {
+            printf("%c", ans[i][j]);
+        }
+    printf("\n");
+    }
+    return 0;
+}

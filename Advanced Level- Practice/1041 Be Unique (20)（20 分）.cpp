879a1ae99bfb6a56319bec074249b047f58c93de
Mr.Huang <18757163093@163.com>
2018-07-27 17:34:52 +0800

    Create 1041 Be Unique (20)（20 分）.cpp

---
+++ Advanced Level- Practice/1041 Be Unique (20)（20 分）.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<map>
+#include<algorithm>
+using namespace std;
+int a[100001], HashTable[10001] = {0};
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+        HashTable[a[i]]++;
+    }
+    int ans = -1;
+    for(int i = 0; i < n; i++) {
+        if(HashTable[a[i]] == 1) {
+            ans = a[i];
+            break;
+        }
+    }
+    if(ans == -1) {
+        printf("None");
+    } else {
+        printf("%d", ans);
+    }
+    return 0;
+}
